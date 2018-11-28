17c8695c3f98a475491f824de67e41126127d636
encomp <edgar.martinez.rico@gmail.com>
2018-11-28 15:10:12 -0800

    Complete.

--- app/src/main/java/com/example/android/shushme/PlaceListAdapter.java
+++ app/src/main/java/com/example/android/shushme/PlaceListAdapter.java
@@ -1,21 +1,5 @@
 package com.example.android.shushme;
 
-/*
-* Copyright (C) 2017 The Android Open Source Project
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*  	http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
-
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -23,73 +7,82 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-public class PlaceListAdapter extends RecyclerView.Adapter<PlaceListAdapter.PlaceViewHolder> {
+import com.google.android.gms.location.places.PlaceBuffer;
 
-    private Context mContext;
+public final class PlaceListAdapter extends RecyclerView.Adapter<PlaceListAdapter.PlaceViewHolder> {
 
-    /**
-     * Constructor using the context and the db cursor
-     *
-     * @param context the calling context/activity
-     */
-    public PlaceListAdapter(Context context) {
-        // TODO (4) Take a PlaceBuffer as an input and store it as a local private member mPlaces
-        this.mContext = context;
+  private Context mContext;
+  private PlaceBuffer placeBuffer;
+
+  /**
+   * Constructor using the context and the db cursor
+   *
+   * @param context the calling context/activity
+   */
+  public PlaceListAdapter(Context context, PlaceBuffer placeBuffer) {
+    this.mContext = context;
+    this.placeBuffer = placeBuffer;
+  }
+
+  /**
+   * Called when RecyclerView needs a new ViewHolder of the given type to represent an item
+   *
+   * @param parent The ViewGroup into which the new View will be added
+   * @param viewType The view type of the new View
+   * @return A new PlaceViewHolder that holds a View with the item_place_card layout
+   */
+  @Override
+  public PlaceViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    // Get the RecyclerView item layout
+    LayoutInflater inflater = LayoutInflater.from(mContext);
+    View view = inflater.inflate(R.layout.item_place_card, parent, false);
+    return new PlaceViewHolder(view);
+  }
+
+  /**
+   * Binds the data from a particular position in the cursor to the corresponding view holder
+   *
+   * @param holder The PlaceViewHolder instance corresponding to the required position
+   * @param position The current position that needs to be loaded with data
+   */
+  @Override
+  public void onBindViewHolder(PlaceViewHolder holder, int position) {
+    // from the Place object at the specified position in mPlaces
+    holder.nameTextView.setText(placeBuffer.get(position).getName());
+    holder.addressTextView.setText(placeBuffer.get(position).getAddress());
+  }
+
+  // with a new one
+  public void swapPlaces(PlaceBuffer placeBuffer) {
+    this.placeBuffer = placeBuffer;
+    if (placeBuffer != null) {
+      notifyDataSetChanged();
     }
+  }
 
-    /**
-     * Called when RecyclerView needs a new ViewHolder of the given type to represent an item
-     *
-     * @param parent   The ViewGroup into which the new View will be added
-     * @param viewType The view type of the new View
-     * @return A new PlaceViewHolder that holds a View with the item_place_card layout
-     */
-    @Override
-    public PlaceViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        // Get the RecyclerView item layout
-        LayoutInflater inflater = LayoutInflater.from(mContext);
-        View view = inflater.inflate(R.layout.item_place_card, parent, false);
-        return new PlaceViewHolder(view);
+  /**
+   * Returns the number of items in the cursor
+   *
+   * @return Number of items in the cursor, or 0 if null
+   */
+  @Override
+  public int getItemCount() {
+    if (placeBuffer == null) {
+      return 0;
     }
+    return placeBuffer.getCount();
+  }
 
-    /**
-     * Binds the data from a particular position in the cursor to the corresponding view holder
-     *
-     * @param holder   The PlaceViewHolder instance corresponding to the required position
-     * @param position The current position that needs to be loaded with data
-     */
-    @Override
-    public void onBindViewHolder(PlaceViewHolder holder, int position) {
-        // TODO (6) Implement onBindViewHolder to set the view holder's Name and Address text fields
-        // from the Place object at the specified position in mPlaces
+  /** PlaceViewHolder class for the recycler view item */
+  class PlaceViewHolder extends RecyclerView.ViewHolder {
+
+    TextView nameTextView;
+    TextView addressTextView;
+
+    public PlaceViewHolder(View itemView) {
+      super(itemView);
+      nameTextView = (TextView) itemView.findViewById(R.id.name_text_view);
+      addressTextView = (TextView) itemView.findViewById(R.id.address_text_view);
     }
-
-    //TODO (7) Implement a public method swapPlaces that replaces the current mPlaces PlaceBuffer with a new one
-
-    /**
-     * Returns the number of items in the cursor
-     *
-     * @return Number of items in the cursor, or 0 if null
-     */
-    @Override
-    public int getItemCount() {
-        // TODO (5) Update getItemCount to return mPlaces's item count
-        return 0;
-    }
-
-    /**
-     * PlaceViewHolder class for the recycler view item
-     */
-    class PlaceViewHolder extends RecyclerView.ViewHolder {
-
-        TextView nameTextView;
-        TextView addressTextView;
-
-        public PlaceViewHolder(View itemView) {
-            super(itemView);
-            nameTextView = (TextView) itemView.findViewById(R.id.name_text_view);
-            addressTextView = (TextView) itemView.findViewById(R.id.address_text_view);
-        }
-
-    }
+  }
 }
